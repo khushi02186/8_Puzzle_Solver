0ad7d4abbc5b7ed3208aecf2053e7d273d57b9c9
khushi02186 <80617183+khushi02186@users.noreply.github.com>
2022-11-22 12:23:46 +0530

    Add files via upload

---
+++ 8_Block_Code/Puzzle.java
@@ -0,0 +1,30 @@
+package pkg8.puzzle;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ * 
+ * contains only the main method that starts the GUI, nothing is done here
+ */
+public class Puzzle {
+
+
+    public static void main(String[] args){
+        
+        //set the look and feel to system's look and feel, who likes java's native look and feel :"D
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //start the gui, let the magic begin :D
+        GUI gui = new GUI();
+        gui.setVisible(true);
+        
+    }
+    
+}
